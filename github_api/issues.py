81d6c60b6d92e32c992e7232e761a7cfbb8768ea
rudehn <nrude93@gmail.com>
2017-05-26 11:25:54 -0400

    bug fix

--- github_api/issues.py
+++ github_api/issues.py
@@ -1,12 +1,12 @@
 
 
-close_issue(api, urn, issue_id):
+def close_issue(api, urn, issue_id):
     path="/repos/{urn}/issues/{issue}".format(urn=urn, issue=issue_id)
     data = {"state": "closed"}
     resp = api("PATCH", path, json=data)
     return resp
     
-open_issue(api, urn, issue_id):
+def open_issue(api, urn, issue_id):
     path="/repos/{urn}/issues/{issue}".format(urn=urn, issue=issue_id)
     data = {"state": "open"}
     resp = api("PATCH", path, json=data)
