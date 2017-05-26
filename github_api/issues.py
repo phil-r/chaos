fe22321d330c0dc8e9729c938a4876b7fa1d0547
rudehn <nrude93@gmail.com>
2017-05-26 12:53:43 -0400

    add issue comment last update time

--- github_api/issues.py
+++ github_api/issues.py
@@ -1,4 +1,4 @@
-
+import arrow
 
 def close_issue(api, urn, issue_id):
     path="/repos/{urn}/issues/{issue}".format(urn=urn, issue=issue_id)
@@ -11,3 +11,28 @@ def open_issue(api, urn, issue_id):
     data = {"state": "open"}
     resp = api("PATCH", path, json=data)
     return resp
+
+def get_issue_comment_last_updated(api, urn, comment):
+    path = "/repos/{urn}/issues/comments/{comment}".format(urn=urn, comment=comment)
+    comment = api("get", path)
+    updated = arrow.get(comment["updated_at"])
+    return updated
+
+def voting_window_remaining_seconds(api, urn, comment, window):
+    """ returns the number of seconds until voting is over.  can be negative,
+    meaning voting has been over for that long """
+    now = arrow.utcnow()
+    issue_comment_updated = get_issue_comment_last_updated(api, urn, comment)
+
+    # this is how many seconds ago the issue comment has been updated.
+    # if we don't have a last update time, we're setting this to negative
+    # infinity, which is a mind-bender, but makes the maths work out
+    elapsed_last_update = -math.inf
+    if pr_updated:
+        elapsed_last_update = (now - issue_comment_updated).total_seconds()
+
+    return window - elapsed_last_update
+
+def is_issue_comment_in_voting_window(api, urn, comment, window):
+    return voting_window_remaining_seconds(api, urn, comment, window) <= 0
+   
