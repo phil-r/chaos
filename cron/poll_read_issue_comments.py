9840633ffc3593c67cbb99445258e3bbdeaede56
rudehn <nrude93@gmail.com>
2017-05-28 12:36:17 -0400

    make travis happy

--- cron/poll_read_issue_comments.py
+++ cron/poll_read_issue_comments.py
@@ -9,18 +9,18 @@ import github_api as gh
 
 THIS_DIR = dirname(abspath(__file__))
 # Hopefully this isn't overwritten on pulls
-SAVED_COMMANDS_FILE = join(THIS_DIR,'..',"server/issue_commands_ran.json")
+SAVED_COMMANDS_FILE = join(THIS_DIR, '..', "server/issue_commands_ran.json")
 if not os.path.exists(SAVED_COMMANDS_FILE):
     '''
         "comment_ids_ran": [
             {
-                "comment_id" : .., 
+                "comment_id" : ..,
                 "chaos_response_id" : ..
-            } 
+            }
         ]
     '''
     with open(SAVED_COMMANDS_FILE, 'w') as f:
-        dummy_data = { "comment_ids_ran": []}
+        dummy_data = {"comment_ids_ran": []}
         json.dump(dummy_data, f)
 
 __log = logging.getLogger("chaosbot")
@@ -37,7 +37,8 @@ Command Syntax
 
 COMMAND_LIST = ["/vote"]
 
-def can_run_vote_command(votes, comment_id):
+
+def can_run_vote_command(api, votes, comment_id):
     json_data = {}
     with open(SAVED_COMMANDS_FILE, 'r') as f:
         json_data = json.load(f)
@@ -47,14 +48,14 @@ def can_run_vote_command(votes, comment_id):
         if comment_id == ids_ran["comment_id"]:
             __log.debug("Already ran commmand")
             return False
-    
+
     # Voting window has passed
     now = arrow.utcnow()
     voting_window = gh.voting.get_voting_window(now)
-    
+
     voting_window_over = gh.issues.is_issue_comment_in_voting_window(api, settings.URN, comment_id,
-                voting_window)
-    if not voting_window_over: # Still voting
+                                                                     voting_window)
+    if not voting_window_over:  # Still voting
         return False
 
     # At least one negative vote will cause vote to not pass
@@ -62,8 +63,9 @@ def can_run_vote_command(votes, comment_id):
         if vote < 0:
             __log.debug("vote less than one")
             return False
-    
+
     return True
+
 
 def update_command_ran(command_id):
     json_data = {}
@@ -78,15 +80,17 @@ def update_command_ran(command_id):
         json_data["comment_ids_ran"].append(data)
         json.dump(json_data, f)
 
+
 def get_command_votes(api, urn, comment_id):
     votes = {}
     for voter, vote in gh.voting.get_comment_reaction_votes(api, urn, comment_id):
         votes[voter] = vote
     return votes
 
+
 def handle_vote_command(api, command, issue_id, comment_id, votes):
     orig_command = command[:]
-    # Check for correct command syntax, ie, subcommands    
+    # Check for correct command syntax, ie, subcommands
     log_warning = False
     if len(command):
         sub_command = command.pop(0)
@@ -97,55 +101,44 @@ def handle_vote_command(api, command, issue_id, comment_id, votes):
             gh.issues.open_issue(api, settings.URN, issue_id)
             update_command_ran(comment_id)
         else:
-            # Other commands have an = in them
-            sub_command = sub_command.split("=")
-            if len(sub_command) > 1:
-                args = sub_command[1:]
-                sub_command = sub_command[0]
-                if sub_command == "label":
-                    pass
-                elif sub_command == "remove-label":
-                    pass
-                elif sub_command == "assign":
-                    pass
-                elif sub_command == "unassign":
-                    pass
-                else:
-                    log_warning = True
-            else:
-                log_warning = True    
+            # Implement other commands
+            pass
     else:
         log_warning = True
-        
+
     if log_warning:
         __log.warning("Unknown issue command syntax: /vote {command}".format(command=orig_command))
+
 
 def handle_comment(api, issue_comment):
     issue_id = issue_comment["issue_id"]
     global_comment_id = issue_comment["global_comment_id"]
     comment_text = issue_comment["comment_text"]
-    
+
     parsed_comment = list(map(lambda x: x.lower().strip(), comment_text.split(' ')))
-    
+
     command = parsed_comment.pop(0)
-    if command in COMMAND_LIST: 
+    if command in COMMAND_LIST:
         votes = get_command_votes(api, settings.URN, global_comment_id)
         # We doin stuff boyz
-        if can_run_vote_command(votes, global_comment_id):
-            __log.debug("Handling issue {issue}: comment {comment}".format(issue=issue_id, comment=comment_text))
-            
+        if can_run_vote_command(api, votes, global_comment_id):
+            __log.debug("Handling issue {issue}: comment {comment}".format(issue=issue_id, 
+                                                                           comment=comment_text))
+
             if command == "/vote":
-                handle_vote_command(api, parsed_comment, issue_id, global_comment_id, votes)
+                handle_vote_command(api, parsed_comment, issue_id, 
+                                    global_comment_id, votes)
+
 
 def poll_read_issue_comments():
     __log.info("looking for issue comments")
 
     api = gh.API(settings.GITHUB_USER, settings.GITHUB_SECRET)
-    
+
     issue_comments = gh.comments.get_all_issue_comments(api, settings.URN)
-    
+
     for issue_comment in issue_comments:
         handle_comment(api, issue_comment)
-        
+
     __log.info("Waiting %d seconds until next scheduled Issue comment polling",
                settings.ISSUE_COMMENT_POLLING_INTERVAL_SECONDS)
--- github_api/issues.py
+++ github_api/issues.py
@@ -1,22 +1,27 @@
 import arrow
+import math
+
 
 def close_issue(api, urn, issue_id):
-    path="/repos/{urn}/issues/{issue}".format(urn=urn, issue=issue_id)
+    path = "/repos/{urn}/issues/{issue}".format(urn=urn, issue=issue_id)
     data = {"state": "closed"}
     resp = api("PATCH", path, json=data)
     return resp
-    
+
+
 def open_issue(api, urn, issue_id):
-    path="/repos/{urn}/issues/{issue}".format(urn=urn, issue=issue_id)
+    path = "/repos/{urn}/issues/{issue}".format(urn=urn, issue=issue_id)
     data = {"state": "open"}
     resp = api("PATCH", path, json=data)
     return resp
+
 
 def get_issue_comment_last_updated(api, urn, comment):
     path = "/repos/{urn}/issues/comments/{comment}".format(urn=urn, comment=comment)
     comment = api("get", path)
     updated = arrow.get(comment["updated_at"])
     return updated
+
 
 def voting_window_remaining_seconds(api, urn, comment, window):
     """ returns the number of seconds until voting is over.  can be negative,
@@ -28,11 +33,11 @@ def voting_window_remaining_seconds(api, urn, comment, window):
     # if we don't have a last update time, we're setting this to negative
     # infinity, which is a mind-bender, but makes the maths work out
     elapsed_last_update = -math.inf
-    if pr_updated:
+    if issue_comment_updated:
         elapsed_last_update = (now - issue_comment_updated).total_seconds()
 
     return window - elapsed_last_update
 
+
 def is_issue_comment_in_voting_window(api, urn, comment, window):
     return voting_window_remaining_seconds(api, urn, comment, window) <= 0
-   
